9edea0977738d0672e1c2f001dd98993177273c6
Rodrigo Sosa <RodrigoSosa95@live.com.mx>
2018-08-10 20:35:59 -0500

    Added tests

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -3,62 +3,65 @@ import request from "supertest";
 import app from "../index";
 import User from "../models/User";
 
-describe("User controller requests", () => {
-    const seed = [
-      new User(
-        {
-          email: "example001@example.com",
-          password: "examplePassword",
-          profile: {
-            names: "user001", lastNames: "last names 001"
+describe("User controller requests...", () => {
+  const requestBody = {
+    email: "email@example.com",
+    password: "placeholderPassword",
+    profile: {
+      names: "example",
+      lastNames: "last names"
+    }
+  };
+
+  after(() => {
+    User.remove({}).then(() => {
+      console.log("User tests done...");
+    });
+  });
+
+  it("Should sign up a user", (done: Mocha.Done) => {
+    request(app)
+      .post("/users/sign-up/")
+      .send(requestBody)
+      .expect(200, done);
+  });
+
+  it("Should sign in a user", (done: Mocha.Done) => {
+    request(app)
+      .post("/users/sign-in/")
+      .send({
+        email: requestBody.email,
+        password: requestBody.password
+      })
+      .expect(200, done);
+  });
+
+  describe("User list endpoint tests...", () => {
+    it("Should return status 200 with a correct token", (done: Mocha.Done) => {
+      const agent = request(app);
+      agent
+        .post("/users/sign-in/")
+        .send({
+          email: requestBody.email,
+          password: requestBody.password
+        })
+        .expect(200)
+        .end((signInError, signInResponse) => {
+          if (!signInError) {
+            const { token } = signInResponse.body;
+            agent
+              .get("/users/users/")
+              .set("Authorization", `Bearer ${token}`)
+              .expect(200, done);
+          } else {
+            done(signInError);
           }
-        }
-      ),
-      new User(
-        {
-          email: "example002@example.com",
-          password: "examplePassword",
-          profile: {
-            names: "user002", lastNames: "last names 002"
-          }
-        }
-      )
-    ];
-
-    before(() => {
-      const [ firstUser, secondUser ] = seed;
-      firstUser.save();
-      secondUser.save();
+        });
     });
 
-    after(() => {
-      User.remove({});
+    it("Should return status 401 with no token", (done: Mocha.Done) => {
+      const agent = request(app);
+      agent.get("/users/users").expect(401, done);
     });
-
-    it("Should sign up a user", (done: Mocha.Done) => {
-      const requestBody = {
-        email: "email@example.com",
-        password: "placeholderPassword",
-        profile: {
-          names: "example",
-          lastNames: "last names"
-        },
-      };
-      request(app).post("/users/sign-up/").send(requestBody).expect(200).end((error, response) => {
-        if (error) return done(error);
-        done();
-      });
-
-    });
-
-    it("Should sign in a user", (done: Mocha.Done) => {
-      const requestBody = {
-        email: "example001@example.com",
-        password: "examplePassword",
-      };
-      request(app).post("/users/sign-in/").send(requestBody).expect(200).end((error, response) => {
-        if (error) return done(error);
-        done();
-      });
-    });
+  });
 });
